e377e7bb7bbbf68d903f2fb0134eac36ec919d70
Joseph S. Cha <josephshincha@gmail.com>
2020-02-05 22:35:17 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles
   dd = directors_database
   index = 0 
   while index < (dd[0][:movies].length) do 
-    puts dd[0][:title]
+    puts dd[0][:movies][index][:title]
     index += 1
   end
 end
